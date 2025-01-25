d85209fbfb2a0c2d638463c0aa1e04076789ef34
klensy <1782831+klensy@users.noreply.github.com>
2025-01-25 08:30:31 +0000

    2025-01-25 1.86.0-nightly (1e9b0177d 2025-01-24)

--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/alloc/sidebar-items1.86.0.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/alloc/sidebar-items1.86.0.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"macro":["format","vec"],"mod":["alloc","borrow","boxed","collections","ffi","fmt","rc","slice","str","string","sync","task","vec"]};
+window.SIDEBAR_ITEMS = {"macro":["format","vec"],"mod":["alloc","borrow","boxed","bstr","collections","ffi","fmt","rc","slice","str","string","sync","task","vec"]};
