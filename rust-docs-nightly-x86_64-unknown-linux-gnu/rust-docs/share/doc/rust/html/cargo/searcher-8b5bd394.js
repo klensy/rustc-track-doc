466ed4d38f902a9658aebcaafcb62630e47cf996
klensy <1782831+klensy@users.noreply.github.com>
2025-03-23 08:32:52 +0000

    2025-03-23 1.87.0-nightly (b48576b4d 2025-03-22)

--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/cargo/searcher-8b5bd394.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/cargo/searcher-8b5bd394.js
@@ -468,12 +468,12 @@ window.search = window.search || {};
         showResults(true);
     }
 
-    fetch(path_to_root + 'searchindex-784a8b36.json')
+    fetch(path_to_root + 'searchindex-56da8f54.json')
         .then(response => response.json())
         .then(json => init(json))        
         .catch(error => { // Try to load searchindex.js if fetch failed
             var script = document.createElement('script');
-            script.src = path_to_root + 'searchindex-e2006dbf.js';
+            script.src = path_to_root + 'searchindex-a22d0e42.js';
             script.onload = () => init(window.search);
             document.head.appendChild(script);
         });
--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/rustc/searcher-8b5bd394.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/rustc/searcher-8b5bd394.js
@@ -468,12 +468,12 @@ window.search = window.search || {};
         showResults(true);
     }
 
-    fetch(path_to_root + 'searchindex-7dbd008e.json')
+    fetch(path_to_root + 'searchindex-fe217c76.json')
         .then(response => response.json())
         .then(json => init(json))        
         .catch(error => { // Try to load searchindex.js if fetch failed
             var script = document.createElement('script');
-            script.src = path_to_root + 'searchindex-d531c49b.js';
+            script.src = path_to_root + 'searchindex-7e360e9d.js';
             script.onload = () => init(window.search);
             document.head.appendChild(script);
         });
