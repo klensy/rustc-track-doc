2ab343a34b6c1ab587fd96f5c4d5704b2316544c
klensy <1782831+klensy@users.noreply.github.com>
2025-03-27 08:35:40 +0000

    2025-03-27 1.87.0-nightly (a2e63569f 2025-03-26)

--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/rustc/searcher-8b5bd394.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/rustc/searcher-8b5bd394.js
@@ -468,12 +468,12 @@ window.search = window.search || {};
         showResults(true);
     }
 
-    fetch(path_to_root + 'searchindex-b3e49894.json')
+    fetch(path_to_root + 'searchindex-bdefd3a7.json')
         .then(response => response.json())
         .then(json => init(json))        
         .catch(error => { // Try to load searchindex.js if fetch failed
             var script = document.createElement('script');
-            script.src = path_to_root + 'searchindex-cd618182.js';
+            script.src = path_to_root + 'searchindex-1437c892.js';
             script.onload = () => init(window.search);
             document.head.appendChild(script);
         });
