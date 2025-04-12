b89da75e4290e367ea45415200410df9761587a1
klensy <1782831+klensy@users.noreply.github.com>
2025-04-12 08:33:05 +0000

    2025-04-12 1.88.0-nightly (d2b3dd7c1 2025-04-11)

--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/rustc/searcher-b074573a.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/rustc/searcher-b074573a.js
@@ -519,7 +519,7 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-da179888.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-e9356b52.js', 'search-index');
 
     // Exported functions
     search.hasFocus = hasFocus;
