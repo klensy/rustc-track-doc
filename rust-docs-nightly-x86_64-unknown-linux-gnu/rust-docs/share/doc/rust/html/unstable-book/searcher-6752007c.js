647e9e91e469258471ef2d0f6b37fc84b506fddb
klensy <1782831+klensy@users.noreply.github.com>
2025-07-08 08:43:12 +0000

    2025-07-08 1.90.0-nightly (a2d45f73c 2025-07-07)

--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/unstable-book/searcher-6752007c.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/unstable-book/searcher-6752007c.js
@@ -524,6 +524,6 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-61b0998c.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-fc1e766e.js', 'search-index');
 
 })(window.search);
