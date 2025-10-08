67a07855616b10576b42ac9b330340d3ee17af70
klensy <1782831+klensy@users.noreply.github.com>
2025-10-08 08:37:33 +0000

    2025-10-08 1.92.0-nightly (f6aa851db 2025-10-07)

--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/clippy/searcher-9aeb6ddf.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/clippy/searcher-9aeb6ddf.js
@@ -436,7 +436,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f6c9f6f2.js',
+                path_to_root + 'searchindex-b361398b.js',
                 'search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/unstable-book/searcher-9aeb6ddf.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/unstable-book/searcher-9aeb6ddf.js
@@ -436,7 +436,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-dfeaf4a6.js',
+                path_to_root + 'searchindex-6a95a5ab.js',
                 'search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
