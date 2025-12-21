5646dc5ab347b9ae27a0c5bbf5b8b301858be77e
klensy <1782831+klensy@users.noreply.github.com>
2025-12-21 08:39:15 +0000

    2025-12-21 1.94.0-nightly (24139cf84 2025-12-20)

--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/rustc/searcher-c2a407aa.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/rustc/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a29d639a.js',
+                path_to_root + 'searchindex-8586b6e5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
