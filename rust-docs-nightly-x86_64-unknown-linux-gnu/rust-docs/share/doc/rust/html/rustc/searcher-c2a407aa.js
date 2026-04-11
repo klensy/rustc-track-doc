bab184f43a6c50ae6189d6d21f43cea12704ba11
klensy <1782831+klensy@users.noreply.github.com>
2026-04-11 09:01:51 +0000

    2026-04-11 1.96.0-nightly (02c7f9bec 2026-04-10)

--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/rustc/searcher-c2a407aa.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/rustc/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-115b942e.js',
+                path_to_root + 'searchindex-f07c7dfc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/unstable-book/searcher-c2a407aa.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/unstable-book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e4b8a352.js',
+                path_to_root + 'searchindex-84ec41dd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
