306d98abfdf22220cbe6b41ef7a0cf86754f9549
klensy <1782831+klensy@users.noreply.github.com>
2026-06-05 11:35:59 +0000

    2026-06-05 1.98.0-nightly (e7815e522 2026-06-04)

--- rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/unstable-book/searcher-09f2665d.js
+++ rust-docs-nightly-x86_64-unknown-linux-gnu/rust-docs/share/doc/rust/html/unstable-book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c29b53b9.js',
+                path_to_root + 'searchindex-0c1c795f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
